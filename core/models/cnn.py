def2952b2717560141c2bd126f568a92c58d2934
Steve Ataucuri <lord.ataucuri@ucsp.edu.pe>
2020-02-13 14:10:01 -0300

    Added support for CNN models

--- core/models/cnn.py
+++ core/models/cnn.py
@@ -2,55 +2,60 @@ __author__ = "Steve Ataucuri"
 __copyright__ = "Sprace.org.br"
 __version__ = "1.0.0"
 
-from keras.layers import Input, Dense, Activation, Dropout, LSTM
-from keras.layers import concatenate, CuDNNLSTM
+from keras.layers import Input, Dense, Activation, Dropout, Flatten
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+
 from keras.models import Model
 
 from .base import BaseModel
 from .timer import Timer
 
 class ModelCNN(BaseModel):
-    """A class for an building and inferencing an lstm model"""
+    """A class for an building and inferencing a base line cnn model"""
  
-    def __init__(self):
-        super().__init__()
- 
-    def build_model(self, configs):
+    def __init__(self, configs):
+        super().__init__(configs)
+        self.c = configs
+
+    def build_model(self):
         timer = Timer()
         timer.start()
+ 
+        print('[Model] Creating model..')   
         
-        print('[Model] Creating model..')    
-
-        # this model is not sequencial
-        self.model = None
+        configs = self.c     
 
         for layer in configs['model']['layers']:
 
             neurons = layer['neurons'] if 'neurons' in layer else None
             dropout_rate = layer['rate'] if 'rate' in layer else None
             activation = layer['activation'] if 'activation' in layer else None
-            return_seq = layer['return_seq'] if 'return_seq' in layer else None
             input_timesteps = layer['input_timesteps'] if 'input_timesteps' in layer else None
             input_features = layer['input_features'] if 'input_features' in layer else None
+            filters = layer['filters'] if 'filters' in layer else None
+            kernel_size = layer['kernel_size'] if 'kernel_size' in layer else None
+            pool_size = layer['pool_size'] if 'pool_size' in layer else None
 
-            #create Neural Network
-            if layer['type'] == 'lstm-paralel':
+            print('input_features %s input_timesteps %s ' % ( input_features, input_timesteps))
 
-                first_input = Input(shape=(input_timesteps, input_features))        
-                first_output = CNN(neurons, return_sequences=return_seq, return_state=False)(first_input)
-
-                second_input = Input(shape=(input_timesteps, 1)) # without number of features, just with input_timesteps
-                second_output = CNN(neurons, return_sequences=return_seq, return_state=False)(second_input)
-
-                output = concatenate([first_output, second_output], axis=-1)
-
+            if layer['type'] == 'cnn':
+                self.model.add(Conv1D(filters=64, kernel_size=2, activation=activation, 
+                    input_shape=(input_timesteps, input_features)))
             if layer['type'] == 'dense':
-                output = Dense(neurons, activation = activation)(output)
+                self.model.add(Dense(neurons, activation=activation))
             if layer['type'] == 'dropout':
-                output = Dropout(0.5)(output)
-
-        self.model = Model(inputs=[first_input, second_input], outputs=output)
-        self.model.compile(loss=configs['model']['loss'], optimizer=configs['model']['optimizer'], metrics=['accuracy'])
+                self.model.add(Dropout(dropout_rate))
+            if layer['type'] == 'maxpooling':
+                self.model.add(MaxPooling1D(pool_size=pool_size))
+            if layer['type'] == 'flatten':                
+                self.model.add(Flatten())
+            if layer['type'] == 'activation':
+                self.model.add(Activation('linear'))
         
-        print('[Model] Model Compiled with structure:', self.model.inputs)      
+        print(self.model.summary())
+        self.model.compile(loss=configs['model']['loss'], optimizer=configs['model']['optimizer'], metrics=['accuracy'])       
+        print('[Model] Model Compiled with structure:', self.model.inputs)
+        self.save_architecture(self.save_fname)     
         timer.stop()
+
